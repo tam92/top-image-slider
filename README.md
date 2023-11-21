155458c27d99eca0af55d05c7d810b07cc4d57cf
Tamara <29234416+tam92@users.noreply.github.com>
2023-11-21 18:58:06 +0100

    Image slider. Changes the image every 5 seconds.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # top-image-slider
 This is an exercise from "The Odin Project", to create an image slider.
+Link: https://www.theodinproject.com/lessons/node-path-javascript-dynamic-user-interface-interactions#image-slider
+
+Changes the image every 5 seconds.
+
+
+# Credits
+
+## Images:
+
+- https://www.publicdomainpictures.net/en/view-image.php?image=31051&picture=space
+- https://www.pexels.com/photo/milky-way-galaxy-during-nighttime-1252890/
+- https://pxhere.com/en/photo/915638
+- https://www.publicdomainpictures.net/pictures/90000/velka/space-nebula.jpg
+- https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/pd36-1-gsfc_20171208_archive_e001757.jpg?w=1000&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=2994cf5640620491443e2fc235a1a8f8
+
+## Icons
+Added ```fill-opacity="0"```, so that the arrows are transparent on the inside.
+- https://publicdomainvectors.org/en/free-clipart/Arrow-pointing-right-vector-image/9192.html
+- https://publicdomainvectors.org/en/free-clipart/Arrow-pointing-left-vector-image/9191.html
--- src/index.js
+++ src/index.js
@@ -1,1 +1,95 @@
 import "./style.css";
+
+const imagesSrc = [
+    '/src/images/img1.jpg',
+    '/src/images/img2.jpg',
+    '/src/images/img3.jpg',
+    '/src/images/img4.jpg',
+    '/src/images/img5.jpg'
+]
+let currentImage = 0;
+
+createNavigationDots(imagesSrc);
+arrowNavigation();
+activeDotNavigation();
+
+// Changes the image every 5 seconds (simulates a click to right arrow)
+setInterval(function() {
+    document.getElementById("next-img-btn").click();
+}, 5000)
+
+/**
+ * Creates the corresponding number of navigation dots to number of images in
+ * the given array, and appends them to the 'navigation-dots' div.
+ * @param {Array} imgArray 
+ */
+function createNavigationDots(imgArray) {
+    const navigationDiv = document.getElementById('navigation-dots');
+    imgArray.forEach((value, index) => {
+        const dot = document.createElement("button");
+        dot.setAttribute('id', 'nav-btn-'+ index);
+        dot.classList.add('navigation-btn')
+        navigationDiv.appendChild(dot);
+        if (index === 0) {
+            dot.classList.add('active') // first element active
+        }
+    })
+}
+
+/**
+ * Adds the events listener to the navigation arrows.
+ */
+function arrowNavigation() {
+    const navigationDots = document.getElementById('navigation-dots').children;
+    const arrowBack = document.getElementById("back-img-btn")
+    const arrowNext = document.getElementById("next-img-btn")
+    
+    arrowBack.addEventListener("click", () => {
+        navigationDots[currentImage].classList.remove('active');
+        if (currentImage === 0) {
+            currentImage = navigationDots.length - 1;
+        } else {
+            currentImage = currentImage - 1;
+        }
+        navigationDots[currentImage].classList.add('active');
+        changeBackgroundImage(currentImage)
+    })
+
+    arrowNext.addEventListener("click", () => {
+        navigationDots[currentImage].classList.remove('active');
+        if (currentImage === navigationDots.length -1) {
+            currentImage = 0;
+        } else {
+            currentImage = currentImage + 1;
+        }
+        navigationDots[currentImage].classList.add('active');
+        changeBackgroundImage(currentImage)
+    })
+}
+
+/**
+ * Changes the active class to the appropiate dot button
+ */
+function activeDotNavigation() {
+    const navigationDots = document.getElementById('navigation-dots').children;
+    for (let index = 0; index < navigationDots.length; index++) {
+        const dot = navigationDots[index];
+        dot.addEventListener('click', () => {
+            for (const d of navigationDots) {   // removes the active class
+                if (d.classList.contains('active')) {
+                    d.classList.remove('active');
+                }
+            }
+            // adds active class to the button clicked
+            dot.classList.add('active')
+            changeBackgroundImage(index)
+            currentImage = index;   // modify current image variable
+        })   
+    }
+}
+
+// Changes the background of the page according to the images array index provided
+function changeBackgroundImage(index) {
+    const container = document.getElementById('container');
+    container.setAttribute("style", "background-image: url(" + imagesSrc[index] + ");" )
+}
